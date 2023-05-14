53312291e8f2d0f8fb5f38777e41392443dc2772
unknown <md.adnan.howlader@g.bracu.ac.bd>
2023-05-14 10:43:36 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+
+
+const App=()=> {
+
+  let [searchTerm, setSearchTerm] = React.useState("nes");//array destructuring
+  
+  const handleSearch=(event)=>{
+     setSearchTerm(event.target.value);
+     console.log(searchTerm)
+  }
+  //create a list of objects that store id and name of nintendo console
+
+  let Consoles = [
+    {
+      id: 1,
+      name: "nes",
+    },
+    {
+      id: 2,
+      name: "switch",
+    },
+    {
+      id: 3,
+      name: "gameboy",
+    },
+    {
+      id: 4,
+      name: "gamecube",
+
+    },
+    {
+      id: 5,
+      name: "wii",
+    },
+
+  ];
+
+
+  
+  const filteredItem=Consoles.filter((Console)=>{
+    return Console.name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+  
+  
+
+
+
+
+
+  return(
+
+    <>
+     <Search onSearch={handleSearch}  search={searchTerm}/>
+     <List list={filteredItem} />
+
+    </>
+  
+ 
   );
+
+ 
+      
+    
+  
 }
 
+
+const List=(prop)=>{
+
+  return (
+   
+    <>
+    {prop.list.map(function(item){
+      
+       
+    
+    
+    
+      return <div key={item.id}> {item.name} </div>
+    })}
+    </>)
+
+  
+}
+
+const Search=(props)=>{
+
+  
+
+
+  // //do something here
+  // const handleChange=(event)=>{
+  //   //updating the states
+  //   setSearchTerm(event.target.value);
+  //   props.onSearch(event);
+  // }
+  
+
+  
+  return (
+     
+  <>
+  <h1>My arcade stores</h1>
+  <label htmlFor="search">Search: </label>
+  <input id="search" type="text" onChange={props.onSearch}  />
+  
+  <p>Searching for <strong>{props.search}</strong></p>
+  
+ 
+
+
+    
+
+
+
+
+</>)};
+
+
+
+
+
+
+
 export default App;
+
+
+

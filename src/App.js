c4657f9570af059de186cf78292d627288f237cc
unknown <md.adnan.howlader@g.bracu.ac.bd>
2023-05-25 17:15:11 +0600

    nothing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import React from 'react';
 
-let Consoles = [
+const items = [
   {
     id: 1,
     name: "nes",
@@ -27,6 +27,16 @@ let Consoles = [
 
 ];
 
+const getAsyncStories=(resolve,reject)=>{
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve({data:{stories:items}});
+    },3000);//3 seconds delay and then resolve the promise
+  });
+
+
+}//returns a promise
+
 const useSemiPersistentState=(key,initialState)=>{
 
   let [value, setvalue] = React.useState(localStorage.getItem(key)||initialState);//array destructuring
@@ -46,13 +56,23 @@ const App=()=> {
 
   
   const [searchTerm,setSearchTerm]=useSemiPersistentState('search','switch'); //array destructuring
-  
+  const [Consoles,setConsoles]=React.useState([]);//array destructuring
   const handleSearch=(event)=>{
      setSearchTerm(event.target.value);
      console.log(searchTerm)
     
   }
+
+  React.useEffect(()=>{
+    getAsyncStories().then((result)=>setConsoles(result.data.stories));
+  },[]);//empty array means it runs only on first render
+  
   //create a list of objects that store id and name of nintendo console
+
+  const handleRemoveItem=(item)=>{//filter out the item that matches the id
+    const newConsoles=Consoles.filter((Console)=>Console.id!==item.id);
+    setConsoles(newConsoles);
+  }
 
  
 
@@ -69,7 +89,7 @@ const App=()=> {
      <InputWithLabel id="search"  onInputChange={handleSearch} value={searchTerm} type="text" isFocused>
          My Video Game store
       </InputWithLabel>
-     <List list={filteredItem}  />
+     <List list={filteredItem} onRemoveItem={handleRemoveItem}  />
      
      
 
@@ -81,14 +101,30 @@ const App=()=> {
 }
 
 
-const List=({list})=>{//destructuring props object
-  return list.map((item)=><Item key={(item.id)} name={item.name} />)};
+const List=({list,onRemoveItem})=>{//destructuring props object
+  return list.map((item)=><Item key={item.id} item={item} onRemoveItem={onRemoveItem} />)};
  
 
 
 
-const Item=({name})=>{//destructuring props object
-  return <li>{name}</li>};
+const Item=({item,onRemoveItem})=>{//destructuring props object
+  
+  
+  function RemoveItem(){
+    onRemoveItem(item);
+  }
+
+ 
+  return <div>
+
+  <span>
+    {item.name}
+
+  </span>
+  <span><button type="button" onClick={()=>onRemoveItem(item)}>Dismiss</button></span>
+  
+  </div>;
+};
 
 
 
@@ -111,7 +147,6 @@ const InputWithLabel=({type,id,value,onInputChange,children,isFocused})=>{//dest
     if(isFocused && inputRef.current){
        //clear the input field
        inputRef.current.focus();
-        inputRef.current.value='';
         
       
     }
@@ -138,6 +173,7 @@ const InputWithLabel=({type,id,value,onInputChange,children,isFocused})=>{//dest
  
 
 </>)};
+
 
 export default App;
 
